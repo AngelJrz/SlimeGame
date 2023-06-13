14d40bb69d116be4867fa60e05a464c98da38b02
Angel Juarez Garcia <Angel_IS2020@hotmail.com>
2023-06-12 21:49:46 -0600

    implementación de dash básico

--- Slime Game/Assets/Scripts/player/Movement.cs
+++ Slime Game/Assets/Scripts/player/Movement.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float salto = 14;
     [SerializeField] private float peso = 8;
+    [SerializeField] private TrailRenderer trailRenderer; 
 
     private Rigidbody2D player;
     private SpriteRenderer spritePlayer;
     private float directionX;
     private bool puedeSaltarNuevamente;
+
+    private bool puedeHacerDash = true;
+    private bool estaHaciendoDash;
+    private float dashPower = 24f;
+    private float tiempoDash = 0.2f;
+    private float dashCoolDown = 1f;
 
 
     void Start()
@@ -27,6 +35,11 @@ public class Movement : MonoBehaviour
 
     private void PlayerAction()
     {
+        if (estaHaciendoDash)
+        {
+            return;
+        }
+
         directionX = Input.GetAxisRaw("Horizontal"); 
         player.velocity = new Vector2(directionX * peso, player.velocity.y);
 
@@ -37,6 +50,11 @@ public class Movement : MonoBehaviour
                 player.velocity = new Vector2(player.velocity.x, salto);
                 puedeSaltarNuevamente = false;
             }
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && puedeHacerDash)
+        {
+            StartCoroutine(Dash());
         }
     }
 
@@ -61,5 +79,33 @@ public class Movement : MonoBehaviour
         {
             puedeSaltarNuevamente = true;
         }
+    }
+
+    private IEnumerator Dash()
+    {
+        puedeHacerDash = false;
+        estaHaciendoDash = true;
+        float originalGravity = player.gravityScale;
+        player.gravityScale = 0f;
+
+        float impulso;
+        if (spritePlayer.flipX == false)
+        {
+            impulso = -(transform.localScale.x * dashPower);
+        }
+        else
+        {
+            impulso = transform.localScale.y * dashPower;
+        }
+        player.velocity = new Vector2(impulso, 0f);
+
+        trailRenderer.emitting = true;
+        yield return new WaitForSeconds(tiempoDash);
+        trailRenderer.emitting = false;
+
+        player.gravityScale = originalGravity;
+        estaHaciendoDash = false;
+        yield return new WaitForSeconds(dashCoolDown);
+        puedeHacerDash = true;
     }
 }
